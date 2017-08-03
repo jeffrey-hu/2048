ee885ba99589edd47702b27e605f80619c02b475
jeffrey-hu <lujef000@gmail.com>
2017-08-03 15:11:50 -0400

    Xcode project just created

---
+++ 2048/2048/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2048
+//
+//  Created by Jeffrey Hu on 8/3/17.
+//  Copyright © 2017 Jeffrey Hu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
